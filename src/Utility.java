63cf074a0753eb81f992a2941b5c7d23d2326e22
Sidney Schultz <tewzdk@gmail.com>
2017-11-23 11:58:59 +0100

    intSvar

--- src/Utility.java
+++ src/Utility.java
@@ -1,6 +1,25 @@
 import java.util.Date;
+import java.util.InputMismatchException;
 
 public class Utility {
-    public void validerIntegerSvar(){}
+    public void validerIntegerSvar(){
+
+        //utility
+        public static int intSvar(){ //undgår exceptions, når brugeren indtaster andet end integers.
+            int intSvar = -1;
+            int n = 0;
+            for(int x=0;x<=n;x++) {
+                try {
+                    intSvar = in.nextInt();
+                } catch (InputMismatchException exception) {
+
+                    System.out.println("Indtast et nummer.");
+                    in.nextLine();
+                }
+            }
+            return intSvar;
+        }
+
+    }
     public void validerDateSvar(){}
 }
